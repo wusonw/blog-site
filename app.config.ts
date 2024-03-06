58c8f12ecbc0f4d1cab4471e343abe031a4159a6
wangwuzhen <wangwuzhen@nuaa.edu.cn>
2024-03-07 00:52:21 +0800

    change: change config

--- app.config.ts
+++ app.config.ts
@@ -2,41 +2,42 @@
 export default defineAppConfig({
   alpine: {
     title: 'Wusonw\'s Blog',
-    description: '',
-    image: {
-      src: '/social-card-preview.png',
-      alt: 'An image showcasing my project.',
-      width: 400,
-      height: 300
-    },
+    description: 'My personal blog site',
+    // image: {
+    //   src: '/social-card-preview.png',
+    //   alt: 'An image showcasing my project.',
+    //   width: 400,
+    //   height: 300
+    // },
     header: {
       position: 'right', // possible value are : | 'left' | 'center' | 'right'
       logo: {
         path: '/logo.svg', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
+        alt: 'wusonw' // alt of the logo
       }
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
+        enabled: false, // possible value are : true | false
         repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
       },
-      navigation: true, // possible value are : true | false
+      navigation: false, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: 'Find me' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
-      linkedin: {
-        icon: 'uil:linkedin',
-        label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/nuxtlabs'
-      }
+      // twitter: 'nuxtlabs',
+      // instagram: 'atinuxt',
+      // linkedin: {
+      //   icon: 'uil:linkedin',
+      //   label: 'LinkedIn',
+      //   href: 'https://www.linkedin.com/company/nuxtlabs'
+      // }
+      github:'wusonw'
     },
-    form: {
-      successMessage: 'Message sent. Thank you!'
-    }
+    // form: {
+    //   successMessage: 'Message sent. Thank you!'
+    // }
   }
 })
